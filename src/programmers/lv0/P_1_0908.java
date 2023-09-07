a799fb754a3c7d38e2c50e95792d442f98e28ad6
insoo <dlstn9808@gmail.com>
2023-09-08 02:16:00 +0900

    lv0_1_0908

---
+++ src/programmers/lv0/P_1_0908.java
@@ -0,0 +1,11 @@
+package programmers.lv0;
+
+import java.util.Scanner;
+
+public class P_1_0908 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
