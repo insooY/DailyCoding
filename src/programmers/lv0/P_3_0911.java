5a419baaf8380a720bb43ee612baeff498515b42
insoo <dlstn9808@gmail.com>
2023-09-11 20:25:02 +0900

    lv0_0911

---
+++ src/programmers/lv0/P_3_0911.java
@@ -0,0 +1,15 @@
+package programmers.lv0;
+
+import java.util.Scanner;
+
+public class P_3_0911 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n ; i++){
+            System.out.print(str);
+        }
+    }
+}
